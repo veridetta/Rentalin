c7c5a25388d3317e45abe5300c8ef428d2f3a081
veridetta <veridetta@gmail.com>
2020-02-18 22:03:16 +0700

    login fix

--- app/src/main/java/com/vrcorp/rentalin/CheckLoginActivity.java
+++ app/src/main/java/com/vrcorp/rentalin/CheckLoginActivity.java
@@ -2,23 +2,39 @@ package com.vrcorp.rentalin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class CheckLoginActivity extends AppCompatActivity {
-
+    SharedPreferences sharedpreferences;
+    String kode_login, id;
+    Boolean session=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_login);
-        Intent belumLogin = new Intent(CheckLoginActivity.this, LoginActivity.class);
-        belumLogin.putExtra("CEK_LOGIN", "tidak");
-        Toast.makeText(CheckLoginActivity.this, "Harap Login Dahulu!",
-                Toast.LENGTH_LONG).show();
-        //SharedPreferences.Editor editor = sharedpreferences.edit();
+        SharedPreferences.Editor editor = sharedpreferences.edit();
         //editor.putBoolean("session_status", true);
-        startActivity(belumLogin);
+        sharedpreferences = getSharedPreferences("rentalin", Context.MODE_PRIVATE);
+        session = sharedpreferences.getBoolean("session_status", false);
+        if(session){
+
+                Intent sudahLogin = new Intent(CheckLoginActivity.this, MainActivity.class);
+                //intent.putExtra(EXTRA_MESSAGE, message);
+                Toast.makeText(CheckLoginActivity.this, "Selamat Datang Kembali",
+                        Toast.LENGTH_LONG).show();
+                startActivity(sudahLogin);
+
+        }else{
+            Intent belumLogin = new Intent(CheckLoginActivity.this, LoginActivity.class);
+            //intent.putExtra(EXTRA_MESSAGE, message);
+            belumLogin.putExtra("CEK_LOGIN", "tidak");
+            Toast.makeText(CheckLoginActivity.this, "Harap Login Dahulu!",
+                    Toast.LENGTH_LONG).show();
+            startActivity(belumLogin);
+        }
     }
 }
---
+++ app/src/main/java/com/vrcorp/rentalin/server/Url.java
@@ -0,0 +1,5 @@
+package com.vrcorp.rentalin.server;
+
+public class Url {
+    public static final String URL = "https://pilm21.my.id/api-rentalin/partner/";
+}
